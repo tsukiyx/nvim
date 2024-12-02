248b29e5ede2de6423cca3370aab2eccfd5cbd51
tsukiyx <noir.xen@gmail.com>
2024-12-02 01:36:57 -0500

    Color scheme kanagawa dragon

--- init.lua
+++ init.lua
@@ -2,4 +2,3 @@ require("core")
 require("notify").setup({
 	background_colour = "#000000",
 })
-vim.cmd.colorscheme('TokyoNight-Night')
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,7 +1,4 @@
-
 vim.g.mapleader = " "
-vim.opt.fillchars = { eob = " " }
 vim.cmd('set relativenumber')
 vim.cmd("set number")
 vim.cmd("set softtabstop=2")
-
