7b83b56a7d8ced635c6b8f7cc6e832a6bf9d10b8
tsukiyx <noir.xen@gmail.com>
2024-12-17 19:45:50 -0500

    formattier react

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,27 +9,15 @@ return {
 			},
 			formatters_by_ft = {
 				lua = { "stylua" },
-
-				-- Python: isort y black
 				python = { "isort", "black" },
-
-				-- Rust: rustfmt, fallback a LSP
 				rust = { "rustfmt", lsp_format = "fallback" },
-
-				-- JavaScript: usa prettierd, luego prettier si no se encuentra el primero
 				javascript = { "prettierd", "prettier", stop_after_first = true },
-
-				-- TypeScript: prettierd y prettier
 				typescript = { "prettierd", "prettier", stop_after_first = true },
-
-				-- Vue: prettierd y prettier para el formateo de archivos Vue
 				vue = { "prettierd", "prettier", "volar", stop_after_first = true },
-
-				-- HTML: prettierd y prettier
 				html = { "prettierd", "prettier", stop_after_first = true },
-
-				-- CSS: prettier para CSS
 				css = { "prettier" },
+				typescriptreact = { "prettier" },
+				javascriptreact = { "prettier" }
 			},
 		})
 	end,
